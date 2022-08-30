c93c39f6e23f976e7d2e6dd54b3d75269da316e4
rakeshpagi <rakesh_prog@yahoo.co.in>
2022-08-30 12:39:25 +0530

    firewalls

--- master-setup.sh
+++ master-setup.sh
@@ -4,6 +4,27 @@ getenforce
 setenforce 0
 sudo sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/' /etc/selinux/config
 
+# ON MASTER NODE 
+firewall-cmd --permanent --add-port=6443/tcp
+firewall-cmd --permanent --add-port=2379-2380/tcp
+firewall-cmd --permanent --add-port=10250/tcp
+firewall-cmd --permanent --add-port=10251/tcp
+firewall-cmd --permanent --add-port=10252/tcp
+firewall-cmd --permanent --add-port=10255/tcp
+firewall-cmd --permanent --add-port=8472/udp
+firewall-cmd --add-masquerade --permanent
+# only if you want NodePorts exposed on control plane IP as well
+firewall-cmd --permanent --add-port=30000-32767/tcp
+
+
+## ON Worker Nodes 
+firewall-cmd --permanent --add-port=10250/tcp
+firewall-cmd --permanent --add-port=10255/tcp
+firewall-cmd --permanent --add-port=8472/udp
+firewall-cmd --permanent --add-port=30000-32767/tcp
+firewall-cmd --add-masquerade --permanent
+
+firewall-cmd --reload
 # Setting Up Kube-Master-1 
 dnf update -y
  
